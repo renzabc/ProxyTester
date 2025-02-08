e16ad486b83ad650ebf200013d3dcdffcb9b298d
renzabc <renzagamanos3@gmail.com>
2025-02-08 00:40:19 -0600

    First Commit

---
+++ proxytester/src/main/java/com/proxytester/proxytester/ProxytesterApplication.java
@@ -0,0 +1,13 @@
+package com.proxytester.proxytester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxytesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxytesterApplication.class, args);
+	}
+
+}
---
+++ proxytester/src/test/java/com/proxytester/proxytester/ProxytesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxytester.proxytester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxytesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
